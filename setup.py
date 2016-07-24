c7fdb16c3b51c711af41857a235ef3b7563d2620
tmancal74 <tmancal74@gmail.com>
2016-07-24 14:31:40 +0200

    Setup update

--- setup.py
+++ setup.py
@@ -59,8 +59,7 @@ setup(
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
+        # Specify the Python versions you support here. In particular, ensure        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
