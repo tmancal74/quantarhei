d1c297645238895d9284f8e6b21d50b4d49a36dd
tmancal74 <tmancal74@gmail.com>
2016-09-06 17:26:54 +0200

    test updates

--- tests/bdd/features/builders/molecules_steps.py
+++ tests/bdd/features/builders/molecules_steps.py
@@ -11,11 +11,10 @@ from quantarhei import Molecule
 
 """ ********************************************************************* """
 
-@step(r'molecule is created')
-def when_Molecule_is_created(self):
-    world.m1 = Molecule("Mol1",[world.e0,world.e1]) 
+""" Feature: Molecule creation """
 
-#@step(r'Given ground state energy is (\d+(?:\.\d+)?) and excited state energy is (\d+(?:\.\d+)?) "([^"]*)"')
+
+"""Given"""
 @step(r'I define a two-level molecule')
 def trans_energies_for_a_molecule(self): #,en0,en1,units):
     # Initialize from the sentence
@@ -31,55 +30,13 @@ def trans_energies_for_a_molecule(self): #,en0,en1,units):
     world.units = units
 
     print("units ",units)
+    
+"""When"""
+@step(r'molecule is created')
+def when_Molecule_is_created(self):
+    world.m1 = Molecule("Mol1",[world.e0,world.e1]) 
 
-
-@step(r'I define two-level molecules')
-def i_define_a_two_level_molecule(self):
-    world.e0 = []
-    world.e1 = []
-    world.units = []
-    for row in self.hashes:
-        world.e0.append(float(row['ground_state_energy']))
-        world.e1.append(float(row['excited_state_energy']))
-        world.units.append(row['units'])
-
-@step(r'molecules are created')
-def when_molecules_are_created(self):
-    N = len(world.e0)
-    molecules = []
-    for i in range(N):
-        e0 = world.e0[i]
-        e1 = world.e1[i]
-        units = world.units[i]
-        m1 = Molecule("Mol",[e0,e1])
-        molecules.append(m1)
-
-    world.molecules = molecules
-
-@step(r'I create transition dipole moment vectors')
-def i_create_tdm_vectors(self):
-    world.dvec = []
-    for row in self.hashes:
-        dx = float(row['dx'])
-        dy = float(row['dy'])
-        dz = float(row['dz'])
-        vec = [dx, dy, dz]
-        world.dvec.append(vec)
-
-@step(r'transition dipole moments are set to molecules')
-def tdm_are_set_to_molecules(self):
-    N = len(world.molecules)
-    for i in range(N):
-        m = world.molecules[i]
-        vec = world.dvec[i]
-        m.set_dipole(0,1,vec)
-
-@step(r'molecules return transition dipole moment vectors')
-def molecules_return_tdm_vectors(self):
-    pass
-
-
-
+"""Then"""
 @step(r"molecule has a correct Hamiltonian with values (\d+(?:\.\d+)?) and (\d+(?:\.\d+)?) in internal units")
 def molecule_has_correct_Hamiltonian(self,val0,val1):
     e0 = float(val0)
@@ -94,10 +51,56 @@ def molecule_has_correct_Hamiltonian(self,val0,val1):
 
     numpy.testing.assert_allclose(data,h.data)
 
-@step(r'I create a transition dipole moment vector')
-def i_create_transition_dipole(self,dx,dy,dz,units):
-    world.tdm = [float(dx),float(dy),float(dz)]
-    world.tdm_units = units
+
+"""Given"""
+@step(r'I define two-level molecules')
+def i_define_a_two_level_molecule(self):
+    world.e0 = []
+    world.e1 = []
+    world.units = []
+    for row in self.hashes:
+        world.e0.append(float(row['ground_state_energy']))
+        world.e1.append(float(row['excited_state_energy']))
+        world.units.append(row['units'])
+
+"""And"""
+@step(r'I create transition dipole moment vectors')
+def i_create_tdm_vectors(self):
+    world.dvec = []
+    for row in self.hashes:
+        dx = float(row['dx'])
+        dy = float(row['dy'])
+        dz = float(row['dz'])
+        vec = [dx, dy, dz]
+        world.dvec.append(vec)
+
+"""When"""
+@step(r'molecules are created')
+def when_molecules_are_created(self):
+    N = len(world.e0)
+    molecules = []
+    for i in range(N):
+        e0 = world.e0[i]
+        e1 = world.e1[i]
+        units = world.units[i]
+        m1 = Molecule("Mol",[e0,e1])
+        molecules.append(m1)
+
+    world.molecules = molecules
+
+"""And"""
+@step(r'transition dipole moments are set to molecules')
+def tdm_are_set_to_molecules(self):
+    N = len(world.molecules)
+    for i in range(N):
+        m = world.molecules[i]
+        vec = world.dvec[i]
+        m.set_dipole(0,1,vec)
+
+"""Then"""
+@step(r'molecules return transition dipole moment vectors')
+def molecules_return_tdm_vectors(self):
+    pass
 
 
  
