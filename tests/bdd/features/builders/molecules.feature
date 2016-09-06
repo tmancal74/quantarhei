4d6546b45eced0d5d2f23d7330e1d25a4165bd74
Tomas Mancal <tmancal74@gmail.com>
2016-09-06 23:59:26 +0200

    test enhancements

--- tests/bdd/features/builders/molecules.feature
+++ tests/bdd/features/builders/molecules.feature
@@ -5,13 +5,33 @@ Feature: Molecule creation
     and asign position to them
 
 Scenario Outline: A user creates a Molecule using different units 
-    Given ground state energy is <ground_state_energy> and excited state energy is <excited_state_energy> "<units>"
-    When Molecule is created
-    Then it has a correct Hamiltonian with values <gse_internal> and <ese_internal> in internal units
+    Given I define a two-level molecule:
+        |  units  |  ground_state_energy  |  excited_state_energy  |
+        | <units> | <ground_state_energy> | <excited_state_energy> |
+    When molecule is created
+    Then molecule has a correct Hamiltonian with values <gse_internal> and <ese_internal> in internal units
 
     Examples:
         |units | ground_state_energy | excited_state_energy | gse_internal | ese_internal |
         | 1/cm |                 0.0 |              12000.0 |          0.0 |      12000.0 |
         | Thz  |                 0.0 |                300.0 |          0.0 |        300.0 |
+
+@in_development
+Scenario Outline: A user adds a transition dipole moment to a molecule
+    Given I define two-level molecules:
+        |ground_state_energy | excited_state_energy | units |
+        |                0.0 |              12000.0 |  1/cm |
+        |                0.0 |                300.0 |  THz  |
+    And I create transition dipole moment vectors:
+        | dx  |  dy |  dz | units |
+        | 0.0 | 0.0 | 1.0 | Debye |
+        | 1.0 | 0.0 | 0.0 | a.u.  |
+    When molecules are created
+    And transition dipole moments are set to molecules
+    Then molecules return transition dipole moment vectors:
+        | dx  |  dy |  dz | units |
+        | 0.0 | 0.0 | 1.0 | Debye |
+        | 1.0 | 0.0 | 0.0 | a.u.  |
+
 
 
--- tests/bdd/features/builders/molecules_steps.py
+++ tests/bdd/features/builders/molecules_steps.py
@@ -11,24 +11,76 @@ from quantarhei import Molecule
 
 """ ********************************************************************* """
 
-@step(r'When Molecule is created')
+@step(r'molecule is created')
 def when_Molecule_is_created(self):
     world.m1 = Molecule("Mol1",[world.e0,world.e1]) 
 
-@step(r'Given ground state energy is (\d+(?:\.\d+)?) and excited state energy is (\d+(?:\.\d+)?) "([^"]*)"')
-def trans_energies_for_a_molecule(self,en0,en1,units):
+#@step(r'Given ground state energy is (\d+(?:\.\d+)?) and excited state energy is (\d+(?:\.\d+)?) "([^"]*)"')
+@step(r'I define a two-level molecule')
+def trans_energies_for_a_molecule(self): #,en0,en1,units):
     # Initialize from the sentence
     # Regular expressions give strings!
-    e0 = float(en0)
-    e1 = float(en1)
+    for row in self.hashes: 
+        e0 = float(row['ground_state_energy'])
+        e1 = float(row['excited_state_energy'])
+        units = row['units']
 
     # save it for other steps
     world.e0 = e0 
     world.e1 = e1
     world.units = units
 
+    print("units ",units)
 
-@step(r"Then it has a correct Hamiltonian with values (\d+(?:\.\d+)?) and (\d+(?:\.\d+)?) in internal units")
+
+@step(r'I define two-level molecules')
+def i_define_a_two_level_molecule(self):
+    world.e0 = []
+    world.e1 = []
+    world.units = []
+    for row in self.hashes:
+        world.e0.append(float(row['ground_state_energy']))
+        world.e1.append(float(row['excited_state_energy']))
+        world.units.append(row['units'])
+
+@step(r'molecules are created')
+def when_molecules_are_created(self):
+    N = len(world.e0)
+    molecules = []
+    for i in range(N):
+        e0 = world.e0[i]
+        e1 = world.e1[i]
+        units = world.units[i]
+        m1 = Molecule("Mol",[e0,e1])
+        molecules.append(m1)
+
+    world.molecules = molecules
+
+@step(r'I create transition dipole moment vectors')
+def i_create_tdm_vectors(self):
+    world.dvec = []
+    for row in self.hashes:
+        dx = float(row['dx'])
+        dy = float(row['dy'])
+        dz = float(row['dz'])
+        vec = [dx, dy, dz]
+        world.dvec.append(vec)
+
+@step(r'transition dipole moments are set to molecules')
+def tdm_are_set_to_molecules(self):
+    N = len(world.molecules)
+    for i in range(N):
+        m = world.molecules[i]
+        vec = world.dvec[i]
+        m.set_dipole(0,1,vec)
+
+@step(r'molecules return transition dipole moment vectors')
+def molecules_return_tdm_vectors(self):
+    pass
+
+
+
+@step(r"molecule has a correct Hamiltonian with values (\d+(?:\.\d+)?) and (\d+(?:\.\d+)?) in internal units")
 def molecule_has_correct_Hamiltonian(self,val0,val1):
     e0 = float(val0)
     e1 = float(val1)
@@ -41,6 +93,13 @@ def molecule_has_correct_Hamiltonian(self,val0,val1):
     data[1,1] = e1 
 
     numpy.testing.assert_allclose(data,h.data)
+
+@step(r'I create a transition dipole moment vector')
+def i_create_transition_dipole(self,dx,dy,dz,units):
+    world.tdm = [float(dx),float(dy),float(dz)]
+    world.tdm_units = units
+
+
  
 """ ********************************************************************** """
 
