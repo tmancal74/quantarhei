16434ef6db86b31a8b9836793047dfe7e796c1a9
Tomas Mancal <tomas@FU-184.karlov.mff.cuni.cz>
2016-09-06 21:57:04 +0200

    improved molecules test

--- tests/bdd/features/builders/molecules.feature
+++ tests/bdd/features/builders/molecules.feature
@@ -1,7 +1,17 @@
 Feature: Molecule creation
 
-Scenario: A user creates a Molecule
-    Given ground state energy is 0.0 and excited state energy is 12000 1/cm
+    As a user I want to create molecules using different units of energy
+    and asign transition dipole moments to them
+    and asign position to them
+
+Scenario Outline: A user creates a Molecule using different units 
+    Given ground state energy is <ground_state_energy> and excited state energy is <excited_state_energy> "<units>"
     When Molecule is created
-    Then it has a correct Hamiltonian
+    Then it has a correct Hamiltonian with values <gse_internal> and <ese_internal> in internal units
+
+    Examples:
+        |units | ground_state_energy | excited_state_energy | gse_internal | ese_internal |
+        | 1/cm |                 0.0 |              12000.0 |          0.0 |      12000.0 |
+        | Thz  |                 0.0 |                300.0 |          0.0 |        300.0 |
+
 
--- tests/bdd/features/builders/molecules_steps.py
+++ tests/bdd/features/builders/molecules_steps.py
@@ -1,16 +1,48 @@
 # -*- coding: utf-8 -*-
 
-import aloe
+from aloe import world
+from aloe import step
+from nose.tools import assert_equal 
 
-@aloe.step(r"When Molecule is created")
+import numpy
+
+from quantarhei import Molecule
+
+
+""" ********************************************************************* """
+
+@step(r'When Molecule is created')
 def when_Molecule_is_created(self):
-    pass
+    world.m1 = Molecule("Mol1",[world.e0,world.e1]) 
 
-@aloe.step(r"Given ground state energy is (\d+(?:\.\d+)? and excited state energy is (\d+(?:\.\d+)? 1/cm")
-def trans_energies_for_a_molecule(self,en0,en1):
-    print(en0, en1)
+@step(r'Given ground state energy is (\d+(?:\.\d+)?) and excited state energy is (\d+(?:\.\d+)?) "([^"]*)"')
+def trans_energies_for_a_molecule(self,en0,en1,units):
+    # Initialize from the sentence
+    # Regular expressions give strings!
+    e0 = float(en0)
+    e1 = float(en1)
 
-@aloe.step(r"Then it has a correct Hamiltonian")
-def molecule_has_correct_Hamiltonian(self):
-    pass
+    # save it for other steps
+    world.e0 = e0 
+    world.e1 = e1
+    world.units = units
+
+
+@step(r"Then it has a correct Hamiltonian with values (\d+(?:\.\d+)?) and (\d+(?:\.\d+)?) in internal units")
+def molecule_has_correct_Hamiltonian(self,val0,val1):
+    e0 = float(val0)
+    e1 = float(val1)
+    # get the Hamiltonian from the Molecule
+    h = world.m1.get_Hamiltonian()
+
+    # This is how the Hamiltonian data shoud look like
+    data = numpy.zeros((2,2),dtype=numpy.float32)
+    data[0,0] = e0
+    data[1,1] = e1 
+
+    numpy.testing.assert_allclose(data,h.data)
+ 
+""" ********************************************************************** """
+
+
 
