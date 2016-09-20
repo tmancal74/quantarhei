e823e5267be99674617477dcb50f86151fb03d8c
tmancal74 <tmancal74@gmail.com>
2016-09-20 12:11:09 +0200

    Correction of AbsSpect demo

--- examples/demo6.py
+++ examples/demo6.py
@@ -5,29 +5,46 @@ import matplotlib.pyplot as plt
 import numpy
 
 ta = qr.TimeAxis(0.0, 1000, 1.0)
+
+"""
+
+    Absorption of a monomeric two-level molecule
+
+
+"""
 cfce_params1 = dict(ftype="OverdampedBrownian",
                    reorg=20.0,
                    cortime=100.0,
-                   T=100,matsubara=20)
+                   T=300,matsubara=20)
 
 with qr.energy_units("1/cm"):
     m = qr.Molecule("Molecule",[0.0,10000.0])
     cfce1 = qr.CorrelationFunction(ta,cfce_params1)
 
-m.set_dipole(0,1,[0.0, 1.0, 0.0])
+    m.set_dipole(0,1,[0.0, 1.0, 0.0])
  
-m.set_egcf((0,1),cfce1)   
+    m.set_egcf((0,1),cfce1)   
     
-a1 = qr.AbsSpect(ta,m)
+    a1 = qr.AbsSpect(ta,m)
 #FIXME: Set this under the units management
-a1.calculate(rwa=10000*qr.core.units.cm2int)
+    a1.calculate(rwa=10000*qr.core.units.cm2int)
 
-#FIXME: Make Abs plotable
-plt.plot(a1.frequency/qr.core.units.cm2int,a1.data,'-r')
+#FIXME: Make AbsSpect plotable and savable (under units management)
+    plt.plot(a1.frequency/qr.core.units.cm2int,a1.data,'-r')
 
-ab = numpy.zeros((len(a1.data),2))
-for kk in range(len(a1.data)):
-    ab[kk,0] = a1.frequency[kk]
-    ab[kk,1] = a1.data[kk]
+save = False
+if save:
+    ab = numpy.zeros((len(a1.data),2))
+    for kk in range(len(a1.data)):
+        ab[kk,0] = a1.frequency[kk]
+        ab[kk,1] = a1.data[kk]
     
-numpy.savetxt("absfile",ab)
+    numpy.savetxt("absfile",ab)
+    
+    
+"""
+
+    Absorption of a simple trimeric aggregate of two-level molecules
+
+
+"""
