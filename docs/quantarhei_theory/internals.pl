dd20d0aeff5ffc264cb1d32f068dbe73e1b71c17
Tomas Mancal <tmancal74@gmail.com>
2016-10-10 11:59:49 +0200

    Added latex2html generated documentation

---
+++ docs/quantarhei_theory/internals.pl
@@ -0,0 +1,14 @@
+# LaTeX2HTML 2012 (1.2)
+# Associate internals original text with physical files.
+
+
+$key = q/eq:C_omega_by_C_2primes/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:C_omega_symmetry/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ docs/quantarhei_theory/labels.pl
@@ -0,0 +1,21 @@
+# LaTeX2HTML 2012 (1.2)
+# Associate labels original text with physical files.
+
+
+$key = q/eq:C_omega_symmetry/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:C_omega_by_C_2primes/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2012 (1.2)
+# labels from external_latex_labels array.
+
+
+1;
+
