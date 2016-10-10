d1eb724a874add9cb89db259b0978fdfeec691c1
Tomas Mancal <tmancal74@gmail.com>
2016-10-10 17:04:51 +0200

    Updated html doc

--- docs/quantarhei_theory/labels.pl
+++ docs/quantarhei_theory/labels.pl
@@ -2,6 +2,10 @@
 # Associate labels original text with physical files.
 
 
+$key = q/eq:H_on_psi/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/eq:C_omega_by_C_2primes/;
 $external_labels{$key} = "$URL/" . q|node9.html|; 
 $noresave{$key} = "$nosave";
@@ -10,16 +14,24 @@ $key = q/eq:C_omega_symmetry/;
 $external_labels{$key} = "$URL/" . q|node9.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/eq:H_on_psi/;
-$external_labels{$key} = "$URL/" . q|node4.html|; 
-$noresave{$key} = "$nosave";
-
 1;
 
 
 # LaTeX2HTML 2012 (1.2)
 # labels from external_latex_labels array.
 
+
+$key = q/eq:C_omega_symmetry/;
+$external_latex_labels{$key} = q|5.8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:C_omega_by_C_2primes/;
+$external_latex_labels{$key} = q|5.9|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:H_on_psi/;
+$external_latex_labels{$key} = q|2.1|; 
+$noresave{$key} = "$nosave";
 
 1;
 
