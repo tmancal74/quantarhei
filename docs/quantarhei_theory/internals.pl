63b6b7ecb161f65321f888a762308d9aabbf40c0
Tomas Mancal <tmancal74@gmail.com>
2016-10-10 17:10:49 +0200

    Update latex documentation

--- docs/quantarhei_theory/internals.pl
+++ docs/quantarhei_theory/internals.pl
@@ -6,12 +6,24 @@ $key = q/eq:H_on_psi/;
 $ref_files{$key} = "$dir".q|node4.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/eq:C_omega_by_C_2primes/;
-$ref_files{$key} = "$dir".q|node9.html|; 
+$key = q/eq:C_omega_symmetry/;
+$ref_files{$key} = "$dir".q|node10.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/eq:C_omega_symmetry/;
-$ref_files{$key} = "$dir".q|node9.html|; 
+$key = q/eq:C_omega_by_C_2primes/;
+$ref_files{$key} = "$dir".q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:bare_2level/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:intro_1el/;
+$ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:Ham_el_1vib/;
+$ref_files{$key} = "$dir".q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 1;
