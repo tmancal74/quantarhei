db2b2d73d02af64194f3f63d221590d8f12891b4
Tomáš Mančal <tomas@Tomass-MacBook-Pro.local>
2017-01-25 22:56:21 +0100

    SpectralDensityDB initialized

--- examples/demo_model_spectdens.py
+++ examples/demo_model_spectdens.py
@@ -9,5 +9,7 @@ axis = TimeAxis(0.0, 10000, 1.0)
 sd = db.get_SpectralDensity(axis, "Wendling_JPCB_104_2000_5825")
 
 with energy_units("1/cm"):
-    sd.plot(axis=[0, 1000, 0.0, numpy.max(sd.data)])
+    sd.plot(axis=[0, 1300, 0.0, numpy.max(sd.data)])
     
+    #fc = sd.get_CorrelationFunction(temperature=300)
+    #fc.plot()
