396cd8f4e9e0ef33108a98e68231ff9902249c35
tmancal74 <tmancal74@gmail.com>
2017-03-02 15:12:14 +0100

    Reorganization of scripts

--- setup.py
+++ setup.py
@@ -110,8 +110,8 @@ setup(
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
-        'console_scripts': ['qrhei=quantarhei.qrhei:main',
-                            'lams=quantarhei.lams:main'],
+        'console_scripts': ['qrhei=quantarhei.scripts.qrhei:main',
+                            'lams=quantarhei.scripts.lams:main'],
     }
     
 )
