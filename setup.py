3122cd817e38fd7ffb213d21d1e37474d08b7ae2
tmancal74 <tmancal74@gmail.com>
2017-09-25 17:34:14 +0200

    Updating dependencies

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy','scipy','matplotlib'],
+    install_requires=['numpy','scipy','matplotlib','h5py'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
