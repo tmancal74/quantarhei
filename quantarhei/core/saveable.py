4124f6b1a6f0346e2cc2409de2f9bf148af82a68
tmancal74 <tmancal74@gmail.com>
2017-10-13 14:39:17 +0200

    Saving in Saveable works
    Save and load methods works with introspection.
    Saving of all simple, numpy and Saveable types works. Saving of lists,
    dicts and tuples is not implemented yet. Neither loading is
    implemented.

--- quantarhei/core/saveable.py
+++ quantarhei/core/saveable.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 import numpy
 import h5py
+import inspect
+import fnmatch
+import re
+from numbers import Number
 
+       
 from .managers import energy_units
 
+def _isattr(obj):
+    return not inspect.ismethod(obj)
+        
 class Saveable:
     """Class implementing object persistence through saving to hdf5 files
     
@@ -46,7 +54,21 @@ class Saveable:
         
             self._do_save(filename=fname, strings=strs, numeric=num, ... )
         
-        
+    
+        >>> class Test(Saveable):
+        ...     a = 0.0
+        ...     def __init__(self):
+        ...       self.b = "ahoj"
+        ...       self.c = ["A","B"]
+        ...       self.d = dict(neco=0.5, jine="ahoj")
+        ...     def set_T(self,TT):
+        ...       self.obj = TT
+
+        >>> T1 = Test()
+        >>> T2 = Test()
+        >>> T1.set_T(T2)
+ 
+       
         """
          
         strings = {}
@@ -55,8 +77,51 @@ class Saveable:
         numdata = {}
         objects = {}
         lists = {}
+        tuples = {}
         dictionaries = {}
         
+        # get all attributes to save
+        
+        rg = fnmatch.translate("__*__")
+        prog = re.compile(rg)
+        
+        info = inspect.getmembers(self, _isattr)
+        attr = []
+        for fo in info:
+            fo_name = fo[0]
+            mch = prog.match(fo_name)
+            if mch is None:
+                attr.append(fo_name)
+
+        for at_name in attr:
+            at = self.__getattribute__(at_name)
+            if isinstance(at,str):
+                #print(at_name, at, "string")
+                strings[at_name] = at
+            elif isinstance(at, Number):
+                #print(at_name, at, "number")
+                numeric[at_name] = at
+            elif isinstance(at, list):
+                #print(at_name, at, "list")
+                lists[at_name] = at
+            elif isinstance(at, bool):
+                #print(at_name, at, "boolean")
+                boolean[at_name] = at
+            elif isinstance(at, tuple):
+                #print(at_name, at, "tuple")
+                tuples[at_name] = at
+            elif isinstance(at, dict):
+                #print(at_name, at, "dictionary")
+                dictionaries[at_name] = at
+            elif isinstance(at, numpy.ndarray):
+                #print(at_name, at, "numpy.array")
+                numdata[at_name] = at
+            elif isinstance(at, Saveable):
+                #print(at_name, at, "Saveable object")
+                objects[at_name] = at
+            else:
+                #print(at_name, at, "unknown") 
+                pass
         
         self._do_save(file=file,
                       strings=strings, 
@@ -65,12 +130,13 @@ class Saveable:
                       numdata=numdata,
                       objects=objects,
                       lists=lists,
+                      tuples=tuples,
                       dictionaries=dictionaries)
                               
             
     def _do_save(self, file="", strings={}, numeric={},
                  boolean={}, numdata={}, objects={},
-                 lists={}, dictionaries={}):
+                 lists={}, dictionaries={}, tuples={}):
         """Performs the save to hdf5 file
         
         
@@ -84,10 +150,10 @@ class Saveable:
         if isinstance(file, str):
             file_openned = True
             f = h5py.File(file,"w")
-            root = self._create_root_group(f,self.__class__)
+            root = self._create_root_group(f,str(self.__class__))
                 
         else:
-            root = self._create_root_group(file,self.__class__)
+            root = self._create_root_group(file,str(self.__class__))
 
         # do the save of all dictionaries
         with energy_units("int"):            
@@ -98,6 +164,7 @@ class Saveable:
             self._save_numdata(root, numdata)
             self._save_objects(root, objects)
             self._save_lists(root, lists)
+            self._save_tuples(root, tuples)
             self._save_dictionaries(root, dictionaries)
     
     
@@ -115,6 +182,7 @@ class Saveable:
         numdata = {}
         objects = {}
         lists = {}
+        tuples = {}
         dictionaries = {}        
         
         self._do_load(file=file,
@@ -124,7 +192,8 @@ class Saveable:
                       numdata=numdata,
                       objects=objects,
                       lists=lists,
-                      dictionaries=dictionaries)
+                      dictionaries=dictionaries,
+                      tuples=tuples)
 
         
         
@@ -132,7 +201,7 @@ class Saveable:
         
     def _do_load(self, file="", strings={}, numeric={},
                  boolean={}, numdata={}, objects={},
-                 lists={}, dictionaries={}):
+                 lists={}, dictionaries={}, tuples={}):
         """Loads data from hdf5 file
         
         
@@ -161,6 +230,7 @@ class Saveable:
         self._load_numdata(root, numdata)
         self._load_objects(root, objects)
         self._load_lists(root, lists)
+        self._load_tuples(root, tuples)
         self._load_dictionaries(root, dictionaries)            
         
         # if file openned here, close it
@@ -185,68 +255,97 @@ class Saveable:
         """Saves a string as "classid" attribute
         
         """
-        loc.attrs.create("classid",classid)
+        loc.attrs.create("classid",numpy.string_(str(classid)))
+        print(classid)
     
     
     def _save_strings(self, loc, dictionary):
         """Saves a dictionary of strings under the group "strings"
         
         """
+        if len(dictionary) == 0: return
         strs = self._create_root_group(loc,"strings")
         for key in dictionary.keys(): 
             strs.attrs.create(key, numpy.string_(dictionary[key]))
+        print(dictionary.keys())
     
     def _save_numeric(self, loc, dictionary):
         """Saves a dictionary of numeric values under the group "numeric"
         
         """
+        if len(dictionary) == 0: return
         strs = self._create_root_group(loc,"numeric")
         for key in dictionary.keys(): 
             strs.attrs.create(key, dictionary[key])
+        print(dictionary.keys())
     
     def _save_boolean(self, loc, dictionary):
         """Saves a dictionary of boolean values under the group "boolean"
         
         """
+        if len(dictionary) == 0: return
         strs = self._create_root_group(loc,"boolean")
         for key in dictionary.keys(): 
             strs.attrs.create(key, dictionary[key])
+        print(dictionary.keys())
             
             
     def _save_numdata(self, loc, dictionary):
         """Saves a dictionary of numpy arrays under the group "numdata"
         
         """
+        if len(dictionary) == 0: return
         strs = self._create_root_group(loc,"numdata")
         for key in dictionary.keys(): 
             strs.create_dataset(key, dictionary[key])
+        print(dictionary.keys())
     
     def _save_objects(self, loc, dictionary):
         """Saves a dictionary of objects under the group "objects"
         
         """
+        if len(dictionary) == 0: return
         strs = self._create_root_group(loc,"objects")
         for key in dictionary.keys(): 
             obj = dictionary[key]
             obj.save(strs)
+        print(dictionary.keys())
     
-    def _save_lists(self, loc, lists):
+    def _save_lists(self, loc, dictionary):
         """Saves a dictionary of lists under the group "lists"
         
         """
+        if len(dictionary) == 0: return
         strs = self._create_root_group(loc,"lists")
-        for key in lists.keys(): 
-            clist = lists[key]
+        for key in dictionary.keys(): 
+            clist = dictionary[key]
             self._save_a_list(strs, clist)
- 
-    def _save_dictionaries(self, loc, dictionaries):
+        print(dictionary.keys())
+
+    def _save_tuples(self, loc, dictionary):
+        """Saves a dictionary of lists under the group "lists"
+        
+        """
+        if len(dictionary) == 0: return
+        strs = self._create_root_group(loc,"tuples")
+        for key in dictionary.keys(): 
+            clist = dictionary[key]
+            self._save_a_tuple(strs, clist)
+        print(dictionary.keys())
+        
+    def _save_dictionaries(self, loc, dictionary):
         """Saves a dictionary of dictionaries under the group "dicts"
         
         """
+        if len(dictionary) == 0: return
         strs = self._create_root_group(loc,"dicts")
-        for key in dictionaries.keys(): 
-            cdict = dictionaries[key]
+        for key in dictionary.keys(): 
+            cdict = dictionary[key]
             self._save_a_dictionary(strs, cdict)           
+        print(dictionary.keys())
+
+
+
             
     def _save_a_list(self, loc, clist):
         """Saves a list of values
@@ -402,4 +501,4 @@ class TestObject(Saveable):
         numeric_attrs = self.load_numeric_attributes(file, numeric_attr_names)
         self.N = numeric_attrs["N"]
         
-                
+           
