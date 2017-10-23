1508b373227a774bd95d15b7c92da945ec519fce
tmancal74 <tmancal74@gmail.com>
2017-10-23 17:14:33 +0200

    AbsSpectrum is Saveable

--- local_tests.sh
+++ local_tests.sh
@@ -30,9 +30,10 @@ export PYTHONPATH=`pwd`
 #nosetests -vs tests/unit/core/frequency_test.py
 #nosetests -vs tests/unit/core/test_dfunction.py
 #nosetests -vs tests/unit/qm/corfunctions/correlationfunctions_test.py
-nosetests -vs tests/unit/builders/test_molecules.py
-nosetests -vs tests/unit/builders/test_aggregates.py
+#nosetests -vs tests/unit/builders/test_molecules.py
+#nosetests -vs tests/unit/builders/test_aggregates.py
+nosetests -vs tests/unit/spectroscopy/abs_test.py
 
-paver
+#paver
 
 
