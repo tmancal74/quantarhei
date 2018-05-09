c05ffd123bfad2003eb3f03b4842aa1881a9ae69
tmancal74 <tmancal74@gmail.com>
2018-05-09 18:45:19 +0200

    Removing matplotlib tests from travis CI

--- pavement.py
+++ pavement.py
@@ -96,8 +96,12 @@ def pylint():
     Default 
 """
 
+# Removing matplotlib tests because it does not work on remote travis CI
+#@needs('matplotlib_tests', 'unit_tests_v',
+#       'doc_tests_v','aloe_tests_v') #, 'pylint')
 @needs('matplotlib_tests', 'unit_tests_v',
        'doc_tests_v','aloe_tests_v') #, 'pylint')
+
 @task
 def default():
     pass
