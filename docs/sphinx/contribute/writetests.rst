79ebecdf04a26e22cfd666e1f66d03fdb32d2d65
tmancal74 <tmancal74@gmail.com>
2018-05-29 12:01:47 +0200

    Test suit main script cleaned up
    Quantarhei tests reorganized and file cleaned up.
    pavement.py file now includes also tests for building the documentation.

--- docs/sphinx/contribute/writetests.rst
+++ docs/sphinx/contribute/writetests.rst
@@ -3,3 +3,55 @@
 How to Write Tests for Quantarhei
 =================================
 
+Every contribution to Quantarhei has to come with appropriate tests. Because
+of Quantarhei's development history, less than a half of current code is
+subject to testing at every build. This will change in near future.
+
+Running Tests
+-------------
+
+To run the tests on Quantarhei current installation, just invoke the `paver`
+command (`paver` package is listed as one of the testing dependencies):
+
+.. code:: bash
+
+    $ paver
+    
+Equivalently, you can use the predefined task *test* the Makefile as:
+
+.. code:: bash
+
+    $ make test
+    
+To run the same tests, but with the output of the scipts printed on the screen
+(normally such output is camptured and only the information about tests is
+printed), you can run:
+
+.. code:: bash
+
+    $ paver verbose
+    
+This runs the same set of tests, except for the capturing option removed.
+    
+Sometimes you want to make sure that tests are run against your latest code
+(it is assumed here that tests run against the installed copy of Quantarhei).
+For this purpose we have a Makefile task *local_tests*, which rebuilds and
+reinstalls Quantarhei, before running tests:
+
+.. code:: bash
+
+    $ make local_tests
+    
+Appart from these predefined tests, you can also run other versions of the
+tests or just partial tests, e.g. on the files that you develop. Consult the
+`pavement.py` file in the root directory of Quantarhei package.
+
+Writting Doc Tests
+------------------
+
+Writting Unit Tests
+-------------------
+
+Writting Acceptance Tests
+-------------------------
+
--- pavement.py
+++ pavement.py
@@ -1,55 +1,327 @@
+"""
+    Quantarhei Package Tests
+    ========================
+    
+    To run the tests, the following packages have to be installed
+    
+    paver (pip install paver)
+    nose (pip install nose)
+    aloe (pip install aloe)
+    pylint (pip install pylint)
+    
+    About our tests
+    ---------------
+    
+    Quantarhei is tested against unit tests run by the `nose` tool, against
+    acceptance tests run by `aloe` tool and against doc tests (run by `nose`).
+    We also calculate coverege using the `coverage` tool and we plan to 
+    check the code by `pylint`.
+    
+    
+    The ultimit goal of testing in 100 % coverage and perfect code style.
+    
+    
+    The following tasks are available. Run them with `paver` as
+    
+    .. code:: bash
+    
+        $ paver TASK_NAME
+        
+    To test the whole suit, just run `paver` without any task.
+
+    .. code:: bash
+
+        $ paver
+        
+        
+        
+    Doctest Tasks
+    -------------
+    
+    doc_tests
+    doc_tests_v
+    doc_tests_vs
+    doc_tests_cov
+    doc_tests_cov_v
+    doc_tests_cov_vs
+        
+    Runs test doc tests. _cov means `--with-coverage` option, _v means
+    with `-v` option for verbose output and _vs means with `-vs` option, i.e.
+    verbose and printing standard output of the tests.
+        
+        
+    Unit tests
+    ----------
+    
+    unit_tests_vs
+    unit_tests_cov_vs
+
+
+    Acceptance tests
+    ----------------
+    
+    aloe_tests_vs
+    aloe_tests_cov_vs
+    
+    
+    Tests to be run during development
+    ----------------------------------
+    
+    doc_dev
+    unit_dev
+    
+    Edit the list of files or directories that you want to test during
+    development.
+    
+
+"""
 from paver.tasks import task
 from paver.tasks import needs
 from paver.easy import sh
 
 
-""" 
-    Particular tasks
-"""
+###############################################################################
+# 
+#     Graphical output testing
+#
+###############################################################################
+
+#
+# TASK: matplotlib_tests
+#
 @task
 def matplotlib_tests():
+    """Tests matplotlib output
+    
+    This test seems not to work on all platforms. 
+    
+    !!!Currently it is not run in automatic build testing!!!
+    
+    """
     sh('cd quantarhei; ./tests.py; cd ..')
 
+
+###############################################################################
+#    
+# Tests of Quantarhei installed examples
+#
+###############################################################################
+
+#
+# TASK: examples
+#
 @task
 def examples():
-    sh('nosetests  -vs tests/unit/wizard/examples/')
+    """Tests if Quantarhei examples run correctly
     
+    """
+    sh('nosetests  -vs tests/unit/wizard/examples/')
+
+    
+###############################################################################
+#
+# Unit tests
+#
+###############################################################################
+
+#
+# TASK: unit_tests_vs
+#
 @task
 def unit_tests_vs():
+    """Performs verbose units tests printing output but without coverage 
+    
+    """
     sh('nosetests -vs tests/unit')
-@task
-def doc_tests_vs():    
-    sh('nosetests --with-doctest -vs quantarhei/core')
 
+#
+# TASK: unit_tests_v
+#
 @task
 def unit_tests_v():
-    sh('nosetests  --with-coverage -v tests/unit')
+    """Performs verbose units tests capturing output but without coverage 
     
+    """
+    sh('nosetests -v tests/unit')
+
+#
+# TASK: unit_tests_cov_vs
+#
+@task
+def unit_tests_cov_vs():
+    """Performs verbose units tests without capturing output and with coverage 
+    
+    """
+    sh('nosetests  --with-coverage -vs tests/unit')    
+
+#
+# TASK: unit_tests_cov_v
+#
+@task
+def unit_tests_cov_v():
+    """Performs verbose units tests capturing output and with coverage 
+    
+    """
+    sh('nosetests  --with-coverage -v tests/unit')     
+
+
+###############################################################################
+#
+#   Doc tests
+#
+###############################################################################
+
+# Doc tests with coverage
+cmdd = 'nosetests --with-doctest'
+covr = ' --with-coverage'
+
+# Directories to test
+dirs = ['core', 'builders', 'qm/corfunctions', 'spectroscopy',
+        'qm/liouvillespace']
+
+# Specific modules to test
+mods = ['qm/propagators/poppropagator.py']
+
+#
+# TASK: doc_tests_cov_vs
+#
+@task
+def doc_tests_cov_vs():    
+    """Verbose doc test with coverage
+    
+    This test runs with coverage in a verbose mode. Output is not captured
+    but printed to the screen.
+ 
+    Run it as
+    
+    .. code:: bash
+        
+        paver doc_tests_cov_vs
+        
+    """
+    tcmd = cmdd+covr+' -vs '
+    _run_tests(tcmd, mods, dirs)
+    
+    
+#
+# TASK: doc_tests_cov_vs
+#    
+@task
+def doc_tests_cov_v():
+    """Verbose doc test with coverage
+    
+    This test runs with coverage in a verbose mode. Output is captured.
+ 
+    Run it as
+    
+    .. code:: bash
+        
+        paver doc_tests_cov_v
+        
+    """
+    tcmd = cmdd+covr+' -v '
+    _run_tests(tcmd, mods, dirs)
+
+#
+# TASK: doc_tests_vs
+#
+@task
+def doc_tests_vs():    
+    """Verbose doc test without coverage
+    
+    This test runs without coverage in a verbose mode. Output is not captured
+    but printed to the screen.
+ 
+    Run it as
+    
+    .. code:: bash
+        
+        paver doc_tests_vs
+        
+    """
+    tcmd = cmdd+' -vs '
+    _run_tests(tcmd, mods, dirs)
+
+#
+# TASK: doc_tests_vs
+#
 @task
 def doc_tests_v():    
-    sh('nosetests --with-coverage --with-doctest -vs quantarhei/core/')
-    sh('nosetests --with-coverage --with-doctest -vs quantarhei/builders/')
-    sh('nosetests --with-coverage --with-doctest -vs quantarhei/qm/corfunctions')
-    sh('nosetests --with-coverage --with-doctest -vs quantarhei/qm/propagators/poppropagator.py')
-    sh('nosetests --with-coverage --with-doctest -vs quantarhei/spectroscopy/')
-    sh('nosetests --with-coverage --with-doctest -vs quantarhei/qm/liouvillespace')
+    """Verbose doc test without coverage, capturing the output
+    
+    This test runs without coverage in a verbose mode. Output is captured.
+ 
+    Run it as
+    
+    .. code:: bash
+        
+        paver doc_tests_v
+        
+    """
+    tcmd = cmdd+' -v '
+    _run_tests(tcmd, mods, dirs) 
+
+
+def _run_tests(tcmd, dirs, mods):
+    """Runs tests on directories and modules
+    
+    """
+    
+    # run on directories
+    for _dir in dirs:
+        sh(tcmd+"quantarhei/"+_dir)
+
+    # run on modules
+    for _mod in mods:
+        sh(tcmd+"quantarhei/"+_mod)
     
     
+    
+###############################################################################
+#
+#   Aloe tests
+#
+###############################################################################
 @task
 def aloe_tests_vs():
     sh("aloe -vs -a !in_development tests/bdd")
 
 @task
 def aloe_tests_v():
+    sh("aloe -v -a !in_development tests/bdd")
+    
+@task
+def aloe_tests_cov_vs():
+    sh('aloe --with-coverage -vs -a !in_development tests/bdd')
+
+@task
+def aloe_tests_cov_v():
     sh('aloe --with-coverage -v -a !in_development tests/bdd')
-    #sh('aloe -v -a absorption tests/bdd')
+
+
+###############################################################################
+#
+#   Documentation build test
+#
+###############################################################################
+    
+#
+# TASK: html
+#
+@task
+def html():
+    sh('cd docs/sphinx/; make html')
     
 
-"""
-    Optional tasks
-"""  
+###############################################################################
+#
+#   Pylint tests
+#
+###############################################################################
 class Runner:  
+    """Class to run pylint tests
     
+    """
     def __init__(self,path):
         self.path = path
         
@@ -62,6 +334,9 @@ class Runner:
     
 @task
 def pylint():
+    """Runs pylint tests on specified files
+    
+    """
 
     path = 'quantarhei/core/'
     r = Runner(path)
@@ -79,7 +354,7 @@ def pylint():
     path = 'quantarhei/qm/hilbertspace'
     r.set_path(path)
 #    r.un('statevector.py')
-    r.un('aggregate_test.py')
+#    r.un('aggregate_test.py')
 
     path = 'quantarhei/qm/liouvillespace'
     r.set_path(path)
@@ -96,54 +371,109 @@ def pylint():
     
     path = 'quantarhei/utils/'
     r.set_path(path)
-    r.un('logging.py')
+#    r.un('logging.py')
 
-"""
-    Default 
-"""
 
-# Removing matplotlib tests because it does not work on remote travis CI
-#@needs('matplotlib_tests', 'unit_tests_v',
-#       'doc_tests_v','aloe_tests_v') #, 'pylint')
-@needs('unit_tests_v',
-       'doc_tests_v','aloe_tests_v') #, 'pylint')
-@task
-def default():
-    pass
-
-@needs('unit_tests_vs','doc_tests_vs','aloe_tests_vs')
-@task
-def vs():
-    pass
-
-@needs('unit_tests_v','aloe_tests_v')
-@task
-def nodoc():
-    pass
-
-@needs('unit_tests_vs','aloe_tests_vs')
-@task
-def nodoc_vs():
-    pass
-
-@needs('unit_tests')
-@task
-def windows():
-    """ On windows, aloe tool does not work. We do only unit tests"""
-    pass
-
-@task
-def dev():
-   #sh('nosetests -vs tests/unit/qm/liouvillespace/test_lindblad.py')
-   #sh('nosetests --with-doctest -vs quantarhei/builders/')
-   #sh('nosetests   --with-doctest -vs quantarhei/spectroscopy/')
-   sh('nosetests   --with-doctest -vs quantarhei/qm/liouvillespace')
-   #sh('nosetests -vs tests/unit/builders/test_aggregates.py')
-   #sh('nosetests -vs tests/unit/qm/corfunctions/cfmatrix_test.py')
-   #sh('nosetests -vs tests/unit/qm/liouvillespace/test_systembathinteraction.py')
-   #sh('nosetests -vs tests/unit/qm/liouvillespace/test_evolutionsuperoperator.py')
-   #sh('nosetests -vs tests/unit/qm/liouvillespace/test_lindblad.py')
+###############################################################################
+#
+#   Development tasks
+#
+#   Edit this if you need to test only particular part
+#   of the code which you develop. Then maybe ignore this file during commits
+#
+###############################################################################
   
+#
+# Targets for doc tests
+#       
+doc_devs = ['quantarhei/qm/liouvillespace']
+
+#
+# Targets for unit tests
+#
+unit_devs = ['tests/unit/qm/liouvillespace/test_evolutionsuperoperator.py']
+
+
+cmd = 'nosetests'
+doct = ' --with-doctest'
+
+#
+# TASK: doc_dev
+#
+@task
+def doc_dev():
+   """Task to run doctests on files under development
+   
+   """
+   tcmd = cmd+doct+' -vs ' 
+   for _dev in doc_devs:
+       sh(tcmd+_dev)
+
+#
+# TASK: unit_dev
+#
+@task
+def unit_dev():
+   """Task to run unit tests on files under development
+   
+   """
+   tcmd = cmd+' -vs ' 
+   for _dev in unit_devs:
+       sh(tcmd+_dev)
+       
+
+###############################################################################
+#  
+#  Codecov (upload of coverage results to codecov.io server)
+#
+###############################################################################
+   
+#
+# TASK: codecov
+#
 @task
 def codecov():
+    """Uploads the results of `coverage` command on the codecov.io server
+    
+    """
     sh('codecov --token=34f2053d-7aa9-4b0c-b776-92d852e597ca')
+
+
+###############################################################################
+#
+#   Collections of tests including default test task
+#
+###############################################################################
+
+# Removing matplotlib tests because it does not work on remote travis CI
+#@needs('matplotlib_tests',
+#       'unit_tests_cov_vs',
+#       'doc_tests_cov_vs',
+#       'aloe_tests_cov_vs',
+#       'pylint')
+      
+#
+# This is called when paver is run without any task
+#
+@needs('unit_tests_cov_v',
+       'doc_tests_cov_v',
+       'aloe_tests_cov_v')
+@task
+def default():
+    """Default paver task
+    
+    """
+    pass
+
+#
+# Verbose version of the default tests
+#
+@needs('unit_tests_cov_vs',
+       'doc_tests_cov_vs',
+       'aloe_tests_cov_vs')
+@task
+def verbose():
+    """Run the default tests but with printing output
+    
+    """
+    pass
