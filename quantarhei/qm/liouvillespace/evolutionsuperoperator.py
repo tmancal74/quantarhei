c2c01f500c545073753a352588487520d2c138b0
Dobran Krasa <tomasmancal@seznam.cz>
2018-06-01 11:20:53 +0200

    Attempt to force CI server to run tests with a prescibed version of numpy

--- quantarhei/qm/liouvillespace/evolutionsuperoperator.py
+++ quantarhei/qm/liouvillespace/evolutionsuperoperator.py
@@ -46,10 +46,10 @@
     quantarhei.ReducedDensityMatrix object
     ======================================
     data = 
-    [[ 0.+0.j  0.+0.j  0.+0.j]
-     [ 0.+0.j  0.+0.j  0.+0.j]
-     [ 0.+0.j  0.+0.j  1.+0.j]]
-
+    [[ 0. 0.j  0. 0.j  0. 0.j]
+     [ 0. 0.j  0. 0.j  0. 0.j]
+     [ 0. 0.j  0. 0.j  1. 0.j]]
+    
     Now we calculate density matrix evolution using a propagator
 
     >>> prop = tagg.get_ReducedDensityMatrixPropagator(time, 
