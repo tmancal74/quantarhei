d567c1944307e3d724f07efbd6ae19abab6d240f
Dobran Krasa <tomasmancal@seznam.cz>
2018-06-19 09:57:01 +0200

    Improvements on Secular class

--- quantarhei/qm/liouvillespace/secular.py
+++ quantarhei/qm/liouvillespace/secular.py
@@ -8,17 +8,78 @@
 
 """
 
+from ...core.managers import Manager
 
 class Secular:
     """Class representing secular Superoperators
 
-    
 
+    
+    
+    Attributes
+    ----------
+    
+    is_secular : bool
+        If True, the superoperator has a secular form and it should be
+        handled like that.
+        
+        
+    Methods
+    -------
+    
+    secularize() :
+        Converts the superoperator into secular form
    
+    
+    Examples
+    --------
+    
+    This class is used as a base class for classis that want to provide 
+    a secular form. The class has to define the attribute as_operators (bool)
+    and it has to be BasisManaged
+    
+    >>> import quantarhei as qr
+    >>> class Test(Secular, qr.core.BasisManaged):
+    ...     def __init__(self):
+    ...         self.as_operators = False
+    ...         self.data = None
+    ...     def _set_population_rates_from_tensor(self):
+    ...         pass
+    ...     def _set_dephasing_rates_from_tensor(self):
+    ...         pass
+    
+    >>> A = Test()
+    >>> A.is_secular
+    False
+    
+    >>> A.secularize()
+    
+    >>> A.is_secular
+    True
+
+    Nothing will be returned below (returns None)
+    
+    >>> A.get_secular_basis_operator()
+
+    If we secularize in certain basis, we remember the operator which 
+    defines the basis
+
+    >>> O1 = qr.qm.SelfAdjointOperator(data=[[0.0, 1.0],[1.0, 0.0]])
+    >>> B = Test()
+    >>> with qr.eigenbasis_of(O1):
+    ...     B.secularize()
+    
+    >>> B.is_secular
+    True
+    
+    >>> O2 = B.get_secular_basis_operator()
+    >>> O1 == O2
+    True
     
     """    
     
     is_secular = False
+    secular_basis_op = None
 
     ###########################################################################
     #
@@ -38,7 +99,7 @@ class Secular:
             
             self.secular_basis_op = self._get_current_basis_op()
         
-            if self._as_operators:
+            if self.as_operators:
                 self._set_population_rates_from_operators()
                 self._set_dephasing_rates_from_operators()        
             else:
@@ -93,8 +154,10 @@ class Secular:
     ###########################################################################
     
     def _get_current_basis_op(self):
+        """Returns the operator which defines the currently used basis
         
-        return None
+        """
+        return Manager().current_basis_operator
     
     
 
