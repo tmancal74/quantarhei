53b0d179a54963b2a4c931ff38e4575d9483bd7f
tmancal74 <tmancal74@gmail.com>
2018-10-31 14:34:06 +0100

    New lineshape definitions for 1D and 2D spectra
    New set of realistic lineshapes to be implemented in MockCalculators

--- quantarhei/spectroscopy/lineshapes.py
+++ quantarhei/spectroscopy/lineshapes.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 #
-# This module defines several normalized lineshape functions
+# This module defines several normalized lineshapes for 1D and 2D spectroscopy
 #
 import numpy
 from scipy import special
 
 from .. import REAL
+from .. import COMPLEX
 
 ###############################################################################
 #
@@ -27,10 +28,17 @@ def lorentzian(omega, cent, gamma):
     
     """
     return (gamma/numpy.pi)/((omega-cent)**2 + gamma**2)
+
+
+def lorentzian_im(omega, cent, gamma):
+    """Imaginary part of a normalized Lorenzian line shape
     
+    """
+    return 1j*((omega-cent)/numpy.pi)/((omega-cent)**2 + gamma**2)
+
 
 def voigt(omega, cent, delta, gamma=0.0):
-    """Normalized Voigt line shape
+    """Normalized Voigt line shape for absorption
     
     """    
     z = (omega - cent + 1j*gamma)*numpy.sqrt(numpy.log(2.0))/delta
@@ -38,6 +46,16 @@ def voigt(omega, cent, delta, gamma=0.0):
     return numpy.sqrt(numpy.log(2.0))*\
                       numpy.real(special.wofz(z)) \
                       /(numpy.sqrt(numpy.pi)*delta)
+
+
+def cvoigt(omega, cent, delta, gamma=0.0):
+    """Complex normalized Voigt line shape
+    
+    """
+    fac = 2.0*numpy.sqrt(numpy.log(2.0))/delta
+    z = (gamma - 1j*(omega - cent))/fac
+    
+    return special.erfcx(z)/(numpy.sqrt(numpy.pi)*fac)
 
 
 ###############################################################################
@@ -48,9 +66,42 @@ def voigt(omega, cent, delta, gamma=0.0):
 
 
 def gaussian2D(omega1, cent1, delta1, omega2, cent2, delta2, corr=0.0):
-    """Two-dimensional Gaussian lineshape
+    """Two-dimensional complex Gaussian lineshape   
     
+    """
+    gamma1 = 0.0
+    gamma2 = 0.0
+    return voigt2D(omega1, cent1, delta1, gamma1,
+                   omega2, cent2, delta2, gamma2, corr=corr)
+
+
+def voigt2D(omega1, cent1, delta1, gamma1, 
+            omega2, cent2, delta2, gamma2, corr=0.0):
+    """Two-dimensional complex Voigt lineshape
     
+    """
+    if corr == 0.0:
+        
+        N1 = omega1.shape[0]
+        N2 = omega2.shape[0]
+        
+        dat1 = cvoigt(omega1, cent1, delta1, gamma1)
+        dat2 = cvoigt(omega2, cent2, delta2, gamma2)
+        
+        data = numpy.zeros((N1, N2), dtype=COMPLEX)  
+
+        for k in range(N1):
+            data[k, :] = dat1[k]*dat2[:]
+        
+    else:
+        
+        raise Exception("Not implemented yet")
+        
+    return data        
+
+
+def lorentzian2D(omega1, cent1, gamma1, omega2, cent2, gamma2, corr=0.0):
+    """Two-dimensional complex Lorentzian lineshape
     
     """
     
@@ -59,11 +110,13 @@ def gaussian2D(omega1, cent1, delta1, omega2, cent2, delta2, corr=0.0):
         N1 = omega1.shape[0]
         N2 = omega2.shape[0]
         
-        dat1 = gaussian(omega1, cent1, delta1)
-        dat2 = gaussian(omega2, cent2, delta2)
+        dat1 = lorentzian(omega1, cent1, gamma1) + \
+               lorentzian_im(omega1, cent1, gamma1)
+        dat2 = lorentzian(omega2, cent2, gamma2) + \
+               lorentzian_im(omega2, cent2, gamma2)
         
-        data = numpy.zeros((N1, N2), dtype=REAL)
-        
+        data = numpy.zeros((N1, N2), dtype=COMPLEX)  
+
         for k in range(N1):
             data[k, :] = dat1[k]*dat2[:]
         
