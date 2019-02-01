dac9a9510f9b73afa2ee580595d8d947099e56b7
Tomáš Mančal <tmancal74@gmail.com>
2019-02-02 00:24:35 +0100

    New example and demo (HEOM)

--- quantarhei/wizard/examples/__init__.py
+++ quantarhei/wizard/examples/__init__.py
@@ -5,6 +5,8 @@ _available_examples = ["ex_001_Molecule_Hamiltonian.py",
                        "ex_006_Absorption_1.py",
                        "ex_010_RedfieldTheory_1.py",
                        "ex_011_LindbladForm_1.py",
+                       "ex_012_Integrodiff.py",
+                       "ex_013_HEOM.py",
                        "ex_020_EvolutionSuperOperator_1.py"]
 
 
