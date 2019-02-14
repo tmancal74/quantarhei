6a95100051ff00365203817a2bb0c4210d94fbde
tmancal74 <tmancal74@gmail.com>
2019-02-14 11:04:47 +0100

    User friendly example for Redfield theory added
    Second example for Redfield theory added. This time it is a more user
    friendly version of the usage through Aggregate object.

--- quantarhei/models/__init__.py
+++ quantarhei/models/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 
+from .modelgenerator import ModelGenerator
--- quantarhei/wizard/examples/__init__.py
+++ quantarhei/wizard/examples/__init__.py
@@ -7,6 +7,8 @@ _available_examples = ["ex_001_Molecule_Hamiltonian.py",
                        "ex_011_LindbladForm_1.py",
                        "ex_012_Integrodiff.py",
                        "ex_013_HEOM.py",
+                       "ex_014_HEOM_rates.py",
+                       "ex_015_RedfieldTheory_2.py",
                        "ex_020_EvolutionSuperOperator_1.py",
                        "ex_050_PDB_FMO1.py"]
 
