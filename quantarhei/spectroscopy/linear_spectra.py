28e193df3d8fe149a88d4449aa7695a28e1aa728
Vladislav Slama <slamavl@gmail.com>
2019-11-05 16:37:20 +0100

    None

---
+++ quantarhei/spectroscopy/linear_spectra.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+    Class representing absorption spectrum
+    
+    
+    
+    Class Details
+    -------------
+
+"""
+
+from .absbase import AbsSpectrumBase
+
+class LinSpectrum(AbsSpectrumBase):
+    """Class representing absorption spectrum
+    
+    
+    """
+    pass
+     
+     
+     
