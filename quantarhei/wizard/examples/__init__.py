d9f9f9d850463ad5d408ece80f05d20cd7fcb396
Tomáš Mančal <tmancal74@gmail.com>
2020-01-29 21:38:40 +0100

    New examples and demos

--- quantarhei/wizard/examples/__init__.py
+++ quantarhei/wizard/examples/__init__.py
@@ -15,6 +15,7 @@ _available_examples = ["ex_001_Molecule_Hamiltonian.py",
                        "ex_016_FoersterTheory_1.py",
                        "ex_020_EvolutionSuperOperator_1.py",
                        "ex_050_PDB_FMO1.py",
+                       "ex_300_ParallelIterators.py",
                        "ex_800_DiagProblem.py",
                        "ex_853_RC.py",
                        "ex_854_2DSpectrum_DimerDisorder.py"]
