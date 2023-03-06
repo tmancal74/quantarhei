f7605487c6786274ddfe3dcfa5a79d508e1f2844
Tomáš Mančal <tmancal74@gmail.com>
2023-03-06 22:23:02 +0100

    Cleaning code of old version of AbsSpectrum

--- tests/unit/spectroscopy/abs_test.py
+++ tests/unit/spectroscopy/abs_test.py
@@ -14,13 +14,14 @@ import numpy
 #import h5py
 import tempfile
 
-from quantarhei.spectroscopy.abs import AbsSpectrumBase, AbsSpectrumDifference
+from quantarhei.spectroscopy.abs2 import AbsSpectrumBase #, AbsSpectrumDifference
 from quantarhei import FrequencyAxis
 from quantarhei import energy_units
-from quantarhei import convert
+#from quantarhei import convert
 
 from quantarhei import AbsSpectrum, AbsSpectrumCalculator
-from quantarhei import Molecule, CorrelationFunction, TimeAxis, Aggregate
+from quantarhei import Molecule, CorrelationFunction, TimeAxis
+#from quantarhei import Aggregate
 
 class TestAbs(unittest.TestCase):
     """Tests for the abs package
@@ -96,53 +97,53 @@ class TestAbs(unittest.TestCase):
         numpy.testing.assert_array_equal(abs1.data, abs2.data)
 
 
-    def test_abs_difference_equal_zero(self):
-        """Testing calculation of zero difference between abs spectra
+    # def test_abs_difference_equal_zero(self):
+    #     """Testing calculation of zero difference between abs spectra
         
-        """
+    #     """
         
-        #
-        #  Test difference = 0
-        # 
-        par = [1.0, 11000.0, 100.0]
-        with energy_units("1/cm"):
-            f2 = self._spectral_shape(self.abss.axis, par) 
+    #     #
+    #     #  Test difference = 0
+    #     # 
+    #     par = [1.0, 11000.0, 100.0]
+    #     with energy_units("1/cm"):
+    #         f2 = self._spectral_shape(self.abss.axis, par) 
         
-            abss2 = AbsSpectrumBase(axis=self.abss.axis, data=f2)
-            ad = AbsSpectrumDifference(target=self.abss, optfce=abss2, 
-                                   bounds=(10100.0, 11900.0))
-        d = ad.difference()
+    #         abss2 = AbsSpectrumBase(axis=self.abss.axis, data=f2)
+    #         ad = AbsSpectrumDifference(target=self.abss, optfce=abss2, 
+    #                                bounds=(10100.0, 11900.0))
+    #     d = ad.difference()
         
-        self.assertAlmostEqual(d, 0.0)
+    #     self.assertAlmostEqual(d, 0.0)
         
         
         
-    def test_abs_difference_formula(self):
-        """Testing formula for calculation of difference between abs spectra
+    # def test_abs_difference_formula(self):
+    #     """Testing formula for calculation of difference between abs spectra
         
-        """
+    #     """
 
-        #
-        #  Test difference formula
-        #
-        par = [1.0, 10900.0, 100.0]
-        with energy_units("1/cm"):
-            f2 = self._spectral_shape(self.abss.axis, par) 
+    #     #
+    #     #  Test difference formula
+    #     #
+    #     par = [1.0, 10900.0, 100.0]
+    #     with energy_units("1/cm"):
+    #         f2 = self._spectral_shape(self.abss.axis, par) 
         
-            abss2 = AbsSpectrumBase(axis=self.abss.axis, data=f2)
-            ad = AbsSpectrumDifference(target=self.abss, optfce=abss2, 
-                                   bounds=(10100.0, 11900.0))
-        d = ad.difference()  
+    #         abss2 = AbsSpectrumBase(axis=self.abss.axis, data=f2)
+    #         ad = AbsSpectrumDifference(target=self.abss, optfce=abss2, 
+    #                                bounds=(10100.0, 11900.0))
+    #     d = ad.difference()  
           
-        #with energy_units("1/cm"):   
-        if True:
-            target = self.abss.data[ad.nl:ad.nu]
-            secabs = abss2.data[ad.nl:ad.nu]
-            x = self.abss.axis.data[ad.nl:ad.nu]
-            d2 = 1000.0*numpy.sum(numpy.abs((target-secabs)**2/
-                                   (x[len(x)-1]-x[0])))
+    #     #with energy_units("1/cm"):   
+    #     if True:
+    #         target = self.abss.data[ad.nl:ad.nu]
+    #         secabs = abss2.data[ad.nl:ad.nu]
+    #         x = self.abss.axis.data[ad.nl:ad.nu]
+    #         d2 = 1000.0*numpy.sum(numpy.abs((target-secabs)**2/
+    #                                (x[len(x)-1]-x[0])))
         
-        self.assertAlmostEqual(d, d2)        
+    #     self.assertAlmostEqual(d, d2)        
         
 #    def test_minimize(self):
 #        """Testing difference minimization using scipy.optimize package
