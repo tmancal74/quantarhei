36b80e66c31cea7612c9b2c4b5200000db7e6ec1
Tomáš Mančal <tmancal74@gmail.com>
2025-03-03 11:48:29 +0100

    Some tests of 2D spectra calculation added

--- tests/unit/spectroscopy/twod_test.py
+++ tests/unit/spectroscopy/twod_test.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+from pathlib import Path
+
 import tempfile
 import os
 
 import numpy
+import numpy.testing as npt
 
 from nose.tools import assert_raises
 
@@ -24,6 +27,7 @@ import matplotlib.pyplot as plt
 
 *******************************************************************************
 """
+TEST_DIR = Path(__file__).parent
 
 
 class TestTwod(unittest.TestCase):
@@ -33,16 +37,25 @@ class TestTwod(unittest.TestCase):
     """
     
     def setUp(self,verbose=False):
-        pass
-
-    """
-    def test_MockTwoD(self):
-
+ 
+        
+        #######################################################################
+        #
+        # Create a system to calculate 2D spectrum with Mock calculator
+        #
+        #######################################################################
+        
+        #
+        # System parameters
+        #
         E0 = 12000.0
         E1 = E0 + 100.0
-        #
-        # Create a system and calculate 2D spectrum
-        #
+        
+        
+        self.E0 = E0
+        self.E1 = E1
+        
+
         with qr.energy_units("1/cm"):
             m1 = qr.Molecule([0.0, E0])
             m2 = qr.Molecule([0.0, E1])
@@ -61,21 +74,33 @@ class TestTwod(unittest.TestCase):
             m1.set_transition_width((0,1), 200)
             m2.set_transition_width((0,1), 200)
             
+        self.agg2D = agg.deepcopy()
         
-        agg2 = agg.deepcopy()
         agg.build(mult=1)
         
-        H = agg.get_Hamiltonian()
+        self.H = agg.get_Hamiltonian()
         
         gammas = [1.0/200.0]
-        with qr.eigenbasis_of(H):
+        with qr.eigenbasis_of(self.H):
             
-            K = qr.qm.ProjectionOperator(1, 2, dim=H.dim)
+            K = qr.qm.ProjectionOperator(1, 2, dim=self.H.dim)
             
         sbi = qr.qm.SystemBathInteraction(sys_operators=[K], rates=gammas,
                                           system=agg)
         
-        LL = qr.qm.LindbladForm(H, sbi)
+        self.LL = qr.qm.LindbladForm(self.H, sbi)
+
+
+
+
+    def test_MockTwoD(self):
+        """Testing calculation with artificial lineshapes
+        
+        """
+        
+        agg2D = self.agg2D
+        H = self.H
+        LL = self.LL            
         
         t2axis = qr.TimeAxis(0.0, 200, 5.0)
         t1axis = qr.TimeAxis(0.0, 100, 10.0)
@@ -85,33 +110,86 @@ class TestTwod(unittest.TestCase):
         eUt.set_dense_dt(10)
         eUt.calculate()
 
-        msc = qr.MockTwoDSpectrumCalculator(t1axis, t2axis, t3axis)
-        msc.bootstrap(rwa=qr.convert((E0+E1)/2.0,"1/cm","int"), 
-                      all_positive=False, shape="Gaussian")
+        msc = qr.MockTwoDResponseCalculator(t1axis, t2axis, t3axis)
+        msc.bootstrap(rwa=qr.convert((self.E0+self.E1)/2.0,"1/cm","int"), 
+                      shape="Gaussian")
     
-        agg2.build(mult=2)
-        agg2.diagonalize()
+        agg2D.build(mult=2)
+        agg2D.diagonalize()
 
         lab = qr.LabSetup()
-        lab.set_polarizations(pulse_polarizations=[X,X,X],
+        lab.set_pulse_polarizations(pulse_polarizations=[X,X,X],
                               detection_polarization=X)   
         
+        #
+        # Calculation one by one
+        # 
         cont = qr.TwoDSpectrumContainer(t2axis=t2axis)
         
         for t2 in t2axis.data:
-            twod = msc.calculate_one_system(t2, agg2, H, eUt, lab) 
+            twod = msc.calculate_one_system(t2, agg2D, eUt, lab) 
         
             cont.set_spectrum(twod)        
         
         cont.set_data_flag(qr.signal_TOTL)
+        
         sp = cont.get_spectrum(800.0)
         
+        file_path_1 = TEST_DIR / "twod_test_sp_800.dat"
+        
+        #
+        # This is how you save the data for future comparison
+        #
+        save_data = False
+        if save_data:
+            sp.save_data(file_path_1)
+                         
+        
+        #
+        # Here we load spectrum for comparison
+        #
+        sp800 = qr.TwoDSpectrum()
+        sp800.load_data(file_path_1)
+        
+        #
+        # At the moment, axes are not saved with the spectrum and we have to
+        # set them manually.
+        #
+        sp800.set_axis_1(t1axis)
+        sp800.set_axis_3(t3axis)
+        
+        """
         with qr.energy_units("1/cm"):
-            sp.plot(show=True)
+            spl.plot(show=True)
             sp.savefig("twod.png")
+        """
+            
+        #
+        # Calculation of all at once
+        #
+        cont2 = msc.calculate_all_system(agg2D, eUt, lab)
+        cont2.set_data_flag(qr.signal_TOTL)
+        sp2 = cont.get_spectrum(800.0)
+        
+        
+        #
+        # comparison of 2D spectra at 800 fs
+        #
+        npt.assert_allclose(sp800.data, sp2.data)
+        npt.assert_allclose(sp800.data, sp.data)
+
+
+        """ 
+        with qr.energy_units("1/cm"):
+            sp2.plot(show=True)
+            sp2.savefig("twod.png")
+            
+        
+            
+       
 #            
 #            cont.make_movie("movie.mp4")
-            
+          
         pevol = cont.get_point_evolution(qr.convert(12600,"1/cm","int"),
                                          qr.convert(11500,"1/cm","int"),
                                          t2axis)
@@ -119,14 +197,13 @@ class TestTwod(unittest.TestCase):
         plt.plot(t2axis.data, pevol)
         plt.show()
         
-        
+
         pars = cont.global_fit_exponential()
         
         print("Optimum: ", pars.x)
         
         raise Exception()
-        
-    """
+        """
         
     def test_TwoDSpectrumBase(self):
         """Testing basic functions of the TwoDSpectrumBase class
