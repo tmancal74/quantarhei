2D spectrum calculation works. Lineshapes need to be fixed for ESA with energy transfer

quantarhei/spectroscopy/response_implementations.py
@@ -36,9 +36,10 @@ def R1g(t2, t1, t3, lab, system, evol, KK):
     band1 = system.get_band(1)
     band0 = system.get_band(0)
     
-    Ut2 = evol[1]
+    Ut2 = evol[2]
+    
     Ut1 = evol[0]
-    Ut3 = evol[2]
+    Ut3 = evol[1]
 
     # dipole arrangemenent type: abba
     F4 = system.get_F4d('abba')
@@ -103,9 +104,10 @@ def R2g(t2, t1, t3, lab, system, evol, KK):
     band1 = system.get_band(1)
     band0 = system.get_band(0)
     
-    Ut2 = evol[1]
+    Ut2 = evol[2]
+    
     Ut1 = evol[0]
-    Ut3 = evol[2]
+    Ut3 = evol[1]
     
     # dipole arrangemenent type: baba
     F4 = system.get_F4d('baba')
@@ -169,9 +171,9 @@ def R3g(t2, t1, t3, lab, system, evol, KK):
     band1 = system.get_band(1)
     band0 = system.get_band(0)
     
-    Ut2 = evol[1]
+    #Ut2 = evol[1]
     Ut1 = evol[0]
-    Ut3 = evol[2]
+    Ut3 = evol[1]
 
     # dipole arrangemenent type: bbaa
     F4 = system.get_F4d('bbaa')
@@ -235,9 +237,9 @@ def R4g(t2, t1, t3, lab, system, evol, KK):
     band1 = system.get_band(1)
     band0 = system.get_band(0)
     
-    Ut2 = evol[1]
+    #Ut2 = evol[2]
     Ut1 = evol[0]
-    Ut3 = evol[2]
+    Ut3 = evol[1]
 
     # dipole arrangemenent type: bbaa
     F4 = system.get_F4d('bbaa')
@@ -305,9 +307,10 @@ def R1f(t2, t1, t3, lab, system, evol, KK):
     N0 = system.Nb[0]
     N1 = system.Nb[1]
     
-    Ut2 = evol[1]
+    Ut2 = evol[2]
+    
     Ut1 = evol[0]
-    Ut3 = evol[2]
+    Ut3 = evol[1]
     
     # dipole arrangemenent type: fbfaba
     F4 = system.get_F4d('fbfaba')
@@ -385,9 +388,10 @@ def R2f(t2, t1, t3, lab, system, evol, KK):
     N0 = system.Nb[0]
     N1 = system.Nb[1]
     
-    Ut2 = evol[1]
+    Ut2 = evol[2]
+    
     Ut1 = evol[0]
-    Ut3 = evol[2]
+    Ut3 = evol[1]
 
     # dipole arrangemenent type: fafbba
     F4 = system.get_F4d('fafbba')
@@ -428,6 +432,283 @@ def R2f(t2, t1, t3, lab, system, evol, KK):
     return np.transpose(ret)
 
 
+def R1g_RL0(t2, t1, t3, lab, system, evol, KK):
+    """ Returns a matrix of the respose function values for given t1 and t3 
+    
+    Parameters:
+    -----------
+    
+    t1 : numpy.array
+        Array of t1 times (must be the same as the t1 axis of the gg object)
+        
+    t2 : float
+        Value of the t2 (waiting) time of the response
+        
+    t3 : numpy.array
+        Array of t3 times (must be the same as the t3 axis of the gg object)
+        
+    system : aggregate or molecule class
+        An object storing all information about the system including 
+        the values of the line shape functions.
+    
+    
+    """
+    import numpy as np
+
+    gg = system.get_lineshape_functions()
+    # Mx = system.get_participation()
+    MM = system.get_weighted_participation()
+    En = system.get_eigenstate_energies()
+    rwa = system.get_RWA_suggestion()
+    #g = 0  # ground state index
+    gg.create_data(reset={'t2':t2}) 
+
+    band1 = system.get_band(1)
+    band0 = system.get_band(0)
+    
+    Ut2 = evol[3]
+    
+    Ut1 = evol[0]
+    Ut3 = evol[1]
+
+    # dipole arrangemenent type: abba
+    F4 = system.get_F4d('bbaa')
+    dfac = np.einsum('i,abi->ab',lab.F4eM4,F4)
+
+    ret = np.zeros((len(t1),len(t3)), dtype=COMPLEX)
+    for g in band0:
+        for aa in band1:
+            a = aa - 1
+            for bb in band1:
+                b = bb - 1
+                    
+                if a != b:
+                    ret += dfac[a,b]*Ut2[b,a]* \
+                        Ut1[a,:][:,None]*Ut3[b,:][None,:]*\
+                    np.exp(
+                        -(np.einsum("i,ij", MM[a,a,:], gg[:,"t1"]))[:,None]
+                        - np.conj((np.einsum("i,ij", MM[b,b,:], gg[:,"t3"]))[None,:])
+                        -1j*(En[aa]-En[g]-rwa)*t1[:,None]
+                        -1j*(En[bb]-En[g]-rwa)*t3[None,:]
+                    )
+
+    return np.transpose(ret)
+           
+                
+
+def R2g_RL0(t2, t1, t3, lab, system, evol, KK):
+    """ Returns a matrix of the respose function values for given t1 and t3 
+    
+    Parameters:
+    -----------
+    
+    t1 : numpy.array
+        Array of t1 times (must be the same as the t1 axis of the gg object)
+        
+    t2 : float
+        Value of the t2 (waiting) time of the response
+        
+    t3 : numpy.array
+        Array of t3 times (must be the same as the t3 axis of the gg object)
+        
+    system : aggregate or molecule class
+        An object storing all information about the system including 
+        the values of the line shape functions.
+    
+    
+    """
+    import numpy as np
+
+    gg = system.get_lineshape_functions()
+    # Mx = system.get_participation()
+    MM = system.get_weighted_participation()
+    En = system.get_eigenstate_energies()
+    rwa = system.get_RWA_suggestion()
+    #g = 0  # ground state index
+    gg.create_data(reset={'t2':t2})
+
+    band1 = system.get_band(1)
+    band0 = system.get_band(0)
+    
+    Ut2 = evol[3]
+    
+    Ut1 = evol[0]
+    Ut3 = evol[1]
+    
+    # dipole arrangemenent type: baba
+    F4 = system.get_F4d('bbaa')
+    dfac = np.einsum('i,abi->ab',lab.F4eM4,F4)
+
+    ret = np.zeros((len(t1),len(t3)), dtype=COMPLEX)
+    for g in band0:
+        for aa in band1:
+            a = aa - 1
+            for bb in band1:  
+                b = bb - 1
+                
+                if a != b:
+                    ret += dfac[a,b]*Ut2[b,a]* \
+                        Ut1[a,:][:,None]*Ut3[b,:][None,:]*\
+                    np.exp(
+                        - np.conj((np.einsum("i,ij", MM[a,a,:], gg[:,"t1"]))[:,None])
+                        - np.conj((np.einsum("i,ij", MM[b,b,:], gg[:,"t3"]))[None,:])
+                        -1j*(En[g]-En[aa]+rwa)*t1[:,None]
+                        -1j*(En[bb]-En[g]-rwa)*t3[None,:]
+                    )
+    
+    return np.transpose(ret)                
+
+
+
+def R1f_RL0(t2, t1, t3, lab, system, evol, KK):
+    """ Returns a matrix of the respose function values for given t1 and t3 
+    
+    Parameters:
+    -----------
+    
+    t1 : numpy.array
+        Array of t1 times (must be the same as the t1 axis of the gg object)
+        
+    t2 : float
+        Value of the t2 (waiting) time of the response
+        
+    t3 : numpy.array
+        Array of t3 times (must be the same as the t3 axis of the gg object)
+        
+    system : aggregate or molecule class
+        An object storing all information about the system including 
+        the values of the line shape functions.
+    
+    
+    """
+    import numpy as np
+    
+    gg = system.get_lineshape_functions()
+    gg.create_data(reset={'t2':t2})
+    
+    # Mx = system.get_participation()
+    MM = system.get_weighted_participation()
+    En = system.get_eigenstate_energies()
+    rwa = system.get_RWA_suggestion()    
+
+    band0 = system.get_band(0)
+    band1 = system.get_band(1)
+    band2 = system.get_band(2)
+
+    N0 = system.Nb[0]
+    N1 = system.Nb[1]
+    
+    Ut2 = evol[3]
+    
+    Ut1 = evol[0]
+    Ut3 = evol[1]
+    
+    # dipole arrangemenent type: fbfaba
+    F4 = system.get_F4d('fbfbaa')
+    dfac = np.einsum('i,fabi->fab',lab.F4eM4,F4)
+    
+    ret = np.zeros((len(t1),len(t3)), dtype=COMPLEX)
+    for g in band0:
+        for ff in band2:
+            f = ff - N1 - N0
+            p = ff - N0
+            for aa in band1:
+                a = aa - N0
+                for bb in band1:
+                    b = bb - N0
+                    
+                    if a != b:
+
+                        ret += -1.0*dfac[f,a,b]*Ut2[a,b]* \
+                                                Ut1[a,:][:,None]*Ut3[b,:][None,:]*\
+                        np.exp(
+                            - (np.einsum("i,ij", MM[a,a,:], gg[:,"t1"]))[:,None]
+                            - (np.einsum("i,ij", MM[p,p,:], gg[:,"t3"]))[None,:]
+                            -1j*(En[aa]-En[g]-rwa)*t1[:,None]
+                            -1j*(En[ff]-En[bb]-rwa)*t3[None,:]
+                        )
+    
+    return np.transpose(ret)
+
+
+def R2f_RL0(t2, t1, t3, lab, system, evol, KK):
+    """ Returns a matrix of the respose function values for given t1 and t3 
+    
+    Parameters:
+    -----------
+    
+    t1 : numpy.array
+        Array of t1 times (must be the same as the t1 axis of the gg object)
+        
+    t2 : float
+        Value of the t2 (waiting) time of the response
+        
+    t3 : numpy.array
+        Array of t3 times (must be the same as the t3 axis of the gg object)
+        
+    system : aggregate or molecule class
+        An object storing all information about the system including 
+        the values of the line shape functions.
+    
+    
+    """
+    import numpy as np
+
+    gg = system.get_lineshape_functions()
+    # Mx = system.get_participation()
+    MM = system.get_weighted_participation()
+    En = system.get_eigenstate_energies()
+    rwa = system.get_RWA_suggestion()
+
+    gg.create_data(reset={'t2':t2})
+
+    band0 = system.get_band(0)
+    band1 = system.get_band(1)
+    band2 = system.get_band(2)
+
+    N0 = system.Nb[0]
+    N1 = system.Nb[1]
+    
+    Ut2 = evol[3]
+    
+    Ut1 = evol[0]
+    Ut3 = evol[1]
+
+    # dipole arrangemenent type: fafbba
+    F4 = system.get_F4d('fbfbaa')
+    dfac = np.einsum('i,fabi->fab',lab.F4eM4,F4)
+
+    ret = np.zeros((len(t1),len(t3)), dtype=COMPLEX)
+    
+    for g in band0:
+        for ff in band2:
+            f = ff - N1 - N0
+            p = ff - N0
+            for aa in band1:
+                a = aa - N0
+                for bb in band1:
+                    b = bb - N0
+                    
+                    if b != a:
+    
+                        ret += -1.0*dfac[f,a,b]*Ut2[b,a]* \
+                                                Ut1[a,:][:,None]*Ut3[a,:][None,:]*\
+                            np.exp(
+                            - (np.einsum("i,ij", MM[p,p,:], gg[:,"t3"]))[None,:]
+                            - np.conj((np.einsum("i,ij", MM[a,a,:], gg[:,"t1"]))[:,None])
+                            -1j*(En[g]-En[aa]+rwa)*t1[:,None]
+                            -1j*(En[ff]-En[bb]-rwa)*t3[None,:]
+                        )
+
+    return np.transpose(ret)
+
+
+
+
+#
+# REGISTRATION COE
+#
+
 dc = dict()
 
 dc["R1g"] = R1g
@@ -436,6 +717,10 @@ dc["R3g"] = R3g
 dc["R4g"] = R4g
 dc["R1f"] = R1f
 dc["R2f"] = R2f
+dc["R1g_RL0"] = R1g_RL0
+dc["R2g_RL0"] = R2g_RL0
+dc["R1f_RL0"] = R1f_RL0
+dc["R2f_RL0"] = R2f_RL0
 
 
 def get_implementation(name):