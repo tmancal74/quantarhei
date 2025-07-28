3aa5692498f2aeaec7767bb1ab61fb3df72590de
Vladislav Slama <slamavl@gmail.com>
2025-07-28 12:43:06 +0200

    Corrected rwa for the hamiltonian

--- quantarhei/qm/hilbertspace/hamiltonian.py
+++ quantarhei/qm/hilbertspace/hamiltonian.py
@@ -73,10 +73,10 @@ class Hamiltonian(SelfAdjointOperator, BasisManaged, EnergyUnitsManaged):
             raise Exception("First element in 'rwa_indices' has to be zero")
         self.rwa_indices = numpy.array(rwa_indices, dtype=int)
         
-        Null_blocks = numpy.sum(self.dim == self.rwa_indices)
+        #Null_blocks = numpy.sum(self.dim == self.rwa_indices)
 
-        #self.Nblocks = len(self.rwa_indices)
-        self.Nblocks = len(self.rwa_indices) - Null_blocks
+        self.Nblocks = len(self.rwa_indices)
+        #self.Nblocks = len(self.rwa_indices) - Null_blocks
         self.rwa_energies = numpy.zeros(self.data.shape[0], dtype=REAL)
         
         # average energies in every block
@@ -92,13 +92,13 @@ class Hamiltonian(SelfAdjointOperator, BasisManaged, EnergyUnitsManaged):
                 for ii in range(self.rwa_indices[block],upper):
                     en_block[block] += self.data[ii,ii]
                     k += 1
-                if k==0:
-                    continue
+                #if k==0:
+                #    continue
                 en_block[block] = en_block[block]/float(k)
                 # set rwa_energies within the block
                 for ii in range(self.rwa_indices[block],upper):
                     self.rwa_energies[ii] = en_block[block]            
-        
+
         # we have information on RWA
         self.has_rwa = True
 
