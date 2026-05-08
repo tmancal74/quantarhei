9bed5934e303d7888da695c38aff5aac5fde848e
Daniel Herman <daniel.herman@protonmail.com>
2026-05-09 00:41:13 +0200

    #367 Extract _dispatch_operator to deduplicate isinstance chains
    Three basis transformation methods (convert_to_ground_vibbasis,
    trace_converted, trace_over_vibrations) repeated the same operator-type
    dispatch logic. Extract into a single _dispatch_operator helper.

--- quantarhei/builders/aggregate_base.py
+++ quantarhei/builders/aggregate_base.py
@@ -2627,6 +2627,41 @@ class AggregateBase(UnitsManaged, Saveable, OpenSystem):
     #
     ###########################################################################
 
+    def _dispatch_operator(
+        self, operator: Any, dim: int, Nt: int | None = None, allow_tdm: bool = False
+    ) -> tuple:
+        n_indices = 2
+        evolution = False
+        whole = False
+        nop: Any = None
+
+        if isinstance(operator, (ReducedDensityMatrix, DensityMatrix)):
+            nop = ReducedDensityMatrix(dim=dim)
+        elif isinstance(operator, Hamiltonian):
+            nop = Hamiltonian(dim=dim)
+        elif allow_tdm and isinstance(operator, TransitionDipoleMoment):
+            nop = TransitionDipoleMoment(dim=dim)
+            n_indices = 3
+        elif isinstance(
+            operator, (ReducedDensityMatrixEvolution, DensityMatrixEvolution)
+        ):
+            if Nt is not None:
+                nop = ReducedDensityMatrix(dim=dim)
+                evolution = True
+            else:
+                nop = ReducedDensityMatrixEvolution(operator.TimeAxis)
+                rhoi = ReducedDensityMatrix(dim=dim)
+                nop.set_initial_condition(rhoi)
+                evolution = True
+                whole = True
+        else:
+            raise Exception(
+                "Operation not implemented for this type: "
+                + operator.__class__.__name__
+            )
+
+        return nop, n_indices, evolution, whole
+
     def convert_to_ground_vibbasis(self, operator: Any, Nt: int | None = None) -> Any:
         """Converts an operator to a ground state vibrational basis repre
 
@@ -2640,45 +2675,10 @@ class AggregateBase(UnitsManaged, Saveable, OpenSystem):
         we can distinguish the vibrational states properly
 
         """
-        n_indices = 2
-        evolution = False
-        whole = False
-        nop: Any = None
         if operator.dim == self.Ntot:
-            if isinstance(operator, ReducedDensityMatrix) or isinstance(
-                operator, DensityMatrix
-            ):
-                nop = ReducedDensityMatrix(dim=self.Ntot)
-
-            elif isinstance(operator, Hamiltonian):
-                nop = Hamiltonian(dim=self.Ntot)
-                nop1 = Hamiltonian(dim=self.Nel)
-
-            elif isinstance(operator, TransitionDipoleMoment):
-                nop = TransitionDipoleMoment(dim=self.Ntot)
-                n_indices = 3
-
-            elif isinstance(operator, ReducedDensityMatrixEvolution) or isinstance(
-                operator, DensityMatrixEvolution
-            ):
-                if Nt is not None:
-                    nop = ReducedDensityMatrix(dim=self.Ntot)
-                    evolution = True
-                    whole = False
-                else:
-                    nop = ReducedDensityMatrixEvolution(operator.TimeAxis)
-                    rhoi = ReducedDensityMatrix(dim=self.Ntot)
-                    # we set zero initial condition, because this initialized
-                    # the data storage
-                    nop.set_initial_condition(rhoi)
-                    evolution = True
-                    whole = True
-
-            else:
-                raise Exception(
-                    "Operation not implemented for this type: "
-                    + operator.__class__.__name__
-                )
+            nop, n_indices, evolution, whole = self._dispatch_operator(
+                operator, dim=self.Ntot, Nt=Nt, allow_tdm=True
+            )
 
             # FIXME: This limitation might not be necessary
             # in the ground states of all monomers, there must be the same
@@ -2789,41 +2789,10 @@ class AggregateBase(UnitsManaged, Saveable, OpenSystem):
 
     def trace_converted(self, operator: Any, Nt: int | None = None) -> Any:
 
-        n_indices = 2
-        evolution = False
-        whole = False
-        nop: Any = None
-
         if operator.dim == self.Ntot:
-            if isinstance(operator, ReducedDensityMatrix) or isinstance(
-                operator, DensityMatrix
-            ):
-                nop = ReducedDensityMatrix(dim=self.Nel)
-
-            elif isinstance(operator, Hamiltonian):
-                nop = Hamiltonian(dim=self.Nel)
-
-            elif isinstance(operator, ReducedDensityMatrixEvolution) or isinstance(
-                operator, DensityMatrixEvolution
-            ):
-                if Nt is not None:
-                    nop = ReducedDensityMatrix(dim=self.Nel)
-                    evolution = True
-                    whole = False
-                else:
-                    nop = ReducedDensityMatrixEvolution(operator.TimeAxis)
-                    rhoi = ReducedDensityMatrix(dim=self.Nel)
-                    # we set zero initial condition, because this initialized
-                    # the data storage
-                    nop.set_initial_condition(rhoi)
-                    evolution = True
-                    whole = True
-
-            else:
-                raise Exception(
-                    "Operation not implemented for this type: "
-                    + operator.__class__.__name__
-                )
+            nop, n_indices, evolution, whole = self._dispatch_operator(
+                operator, dim=self.Nel, Nt=Nt
+            )
 
             if n_indices == 2:
                 for n in range(self.Nel):
@@ -2847,41 +2816,10 @@ class AggregateBase(UnitsManaged, Saveable, OpenSystem):
         we can distinguish the vibrational states properly
 
         """
-        n_indices = 2
-        evolution = False
-        whole = False
-        nop: Any = None
-
         if operator.dim == self.Ntot:
-            if isinstance(operator, ReducedDensityMatrix) or isinstance(
-                operator, DensityMatrix
-            ):
-                nop = ReducedDensityMatrix(dim=self.Nel)
-
-            elif isinstance(operator, Hamiltonian):
-                nop = Hamiltonian(dim=self.Nel)
-
-            elif isinstance(operator, ReducedDensityMatrixEvolution) or isinstance(
-                operator, DensityMatrixEvolution
-            ):
-                if Nt is not None:
-                    nop = ReducedDensityMatrix(dim=self.Nel)
-                    evolution = True
-                    whole = False
-                else:
-                    nop = ReducedDensityMatrixEvolution(operator.TimeAxis)
-                    rhoi = ReducedDensityMatrix(dim=self.Nel)
-                    # we set zero initial condition, because this initialized
-                    # the data storage
-                    nop.set_initial_condition(rhoi)
-                    evolution = True
-                    whole = True
-
-            else:
-                raise Exception(
-                    "Operation not implemented for this type: "
-                    + operator.__class__.__name__
-                )
+            nop, n_indices, evolution, whole = self._dispatch_operator(
+                operator, dim=self.Nel, Nt=Nt
+            )
 
             if n_indices == 2:
                 # convert to representation by ground-state oscillator
